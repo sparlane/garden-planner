87aef5965654c8fbf7b21f4d997447eebbdd3a64
Scott Parlane <scott@parlanenz.com>
2024-07-06 12:29:05 +1200

    Add planting area types to garden beds

--- garden/models.py
+++ garden/models.py
@@ -27,3 +27,27 @@ class GardenBed(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class GardenRow(models.Model):
+    """
+    A Row in a garden bed
+    """
+    bed = models.ForeignKey(GardenBed, on_delete=models.PROTECT)
+    name = models.TextField(max_length=1024)
+    placement_x = models.IntegerField()
+    placement_y = models.IntegerField()
+    size_x = models.IntegerField()
+    size_y = models.IntegerField()
+
+
+class GardenSquare(models.Model):
+    """
+    A square in a garden bed
+    """
+    bed = models.ForeignKey(GardenBed, on_delete=models.PROTECT)
+    name = models.TextField(max_length=1024)
+    placement_x = models.IntegerField()
+    placement_y = models.IntegerField()
+    size_x = models.IntegerField()
+    size_y = models.IntegerField()
--- garden/rest.py
+++ garden/rest.py
@@ -1,6 +1,6 @@
 from rest_framework import routers, serializers, viewsets
 
-from .models import GardenArea, GardenBed
+from .models import GardenArea, GardenBed, GardenRow, GardenSquare
 
 
 class GardenAreaSerializer(serializers.HyperlinkedModelSerializer):
@@ -15,6 +15,18 @@ class GardenBedSerializer(serializers.HyperlinkedModelSerializer):
         fields = ['area', 'name', 'placement_x', 'placement_y', 'size_x', 'size_y']
 
 
+class GardenRowSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = GardenRow
+        fields = ['bed', 'name', 'placement_x', 'placement_y', 'size_x', 'size_y']
+
+
+class GardenSquareSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = GardenSquare
+        fields = ['bed', 'name', 'placement_x', 'placement_y', 'size_x', 'size_y']
+
+
 class GardenAreaViewSet(viewsets.ModelViewSet):
     queryset = GardenArea.objects.all()
     serializer_class = GardenAreaSerializer
@@ -25,6 +37,18 @@ class GardenBedViewSet(viewsets.ModelViewSet):
     serializer_class = GardenBedSerializer
 
 
+class GardenRowViewSet(viewsets.ModelViewSet):
+    queryset = GardenRow.objects.all()
+    serializer_class = GardenRowSerializer
+
+
+class GardenSquareViewSet(viewsets.ModelViewSet):
+    queryset = GardenSquare.objects.all()
+    serializer_class = GardenSquareSerializer
+
+
 router = routers.DefaultRouter()
 router.register(r'areas', GardenAreaViewSet)
 router.register(r'beds', GardenBedViewSet)
+router.register(r'rows', GardenRowViewSet)
+router.register(r'squares', GardenSquareViewSet)
