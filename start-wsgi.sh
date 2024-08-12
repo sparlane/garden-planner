c8772bd68411ca6804025995e1aa364613112363
Scott Parlane <scott@parlanenz.com>
2024-08-12 20:45:38 +1200

    Switch to using the wsgi protocol instead of http

--- start-wsgi.sh
+++ start-wsgi.sh
@@ -3,4 +3,4 @@
 source venv/bin/activate
 
 ./manage.py migrate
-uwsgi --socket 127.0.0.1:8080 --protocol=http -w gp.wsgi -M -p 10 --vacuum
+uwsgi --socket 127.0.0.1:8080 -w gp.wsgi -M -p 10 --vacuum
