e4f49bc048567a37bee0c3013999e57885743712
Scott Parlane <scott@parlanenz.com>
2025-12-31 11:07:50 +1300

    Require authentication to access garden planner

--- frontend/views.py
+++ frontend/views.py
@@ -2,8 +2,10 @@
 Views to present the frontend
 """
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 
 
+@login_required
 def main(request):
     """
     Show the frontend
