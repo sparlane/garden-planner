5f8237c278b4d056edf0b2da4029f88294b34d01
Scott Parlane <scott@parlanenz.com>
2026-01-23 23:41:11 +1300

    Add functions to add planting api

--- frontend/js/api/plantings.ts
+++ frontend/js/api/plantings.ts
@@ -1,24 +1,51 @@
-import { fetchAsJson } from '../utils'
-import { GardenRowDirectPlanting, GardenSquareDirectPlanting, SeedTrayPlanting, GardenSquareTransplanting, GardenSquarePlanting } from '../types/plantings'
+import { fetchAsJson, csrfPost } from '../utils'
+import {
+  GardenRowDirectPlanting,
+  GardenSquareDirectPlanting,
+  SeedTrayPlanting,
+  GardenSquareTransplanting,
+  GardenSquarePlanting,
+  GardenRowDirectPlantingCreate,
+  GardenSquareDirectPlantingCreate,
+  GardenSquareTransplantingCreate,
+  SeedTrayPlantingCreate,
+  SeedTrayPlantingDetails
+} from '../types/plantings'
 
 function getPlantingDirectSowGardenRows(): Promise<Array<GardenRowDirectPlanting>> {
   return fetchAsJson<Array<GardenRowDirectPlanting>>('/plantings/directsowgardenrow/')
+}
+
+function addPlantingDirectSowGardenRow(data: GardenRowDirectPlantingCreate) {
+  return csrfPost('/plantings/directsowgardenrow/', data)
 }
 
 function getPlantingDirectSowGardenSquares(): Promise<Array<GardenSquareDirectPlanting>> {
   return fetchAsJson<Array<GardenSquareDirectPlanting>>('/plantings/directsowgardensquare/')
 }
 
+function addPlantingDirectSowGardenSquare(data: GardenSquareDirectPlantingCreate) {
+  return csrfPost('/plantings/directsowgardensquare/', data)
+}
+
 function getPlantingSeedTrays(): Promise<Array<SeedTrayPlanting>> {
   return fetchAsJson<Array<SeedTrayPlanting>>('/plantings/seedtray/')
+}
+
+function addPlantingSeedTray(data: SeedTrayPlantingCreate) {
+  return csrfPost('/plantings/seedtray/', data)
 }
 
 function getPlantingTransplantedGardenSquares(): Promise<Array<GardenSquareTransplanting>> {
   return fetchAsJson<Array<GardenSquareTransplanting>>('/plantings/transplantedgardensquare/')
 }
 
-function getPlantingSeedTrayCurrent(): Promise<Array<SeedTrayPlanting>> {
-  return fetchAsJson<Array<SeedTrayPlanting>>('/plantings/seedtray/current/')
+function addPlantingTransplantedGardenSquare(data: GardenSquareTransplantingCreate) {
+  return csrfPost('/plantings/transplantedgardensquare/', data)
+}
+
+function getPlantingSeedTrayCurrent(): Promise<Array<SeedTrayPlantingDetails>> {
+  return fetchAsJson<{ plantings: Array<SeedTrayPlantingDetails> }>('/plantings/seedtray/current/').then((data) => data.plantings)
 }
 
 function getPlantingGardenSquaresCurrent(): Promise<Array<GardenSquarePlanting>> {
@@ -27,9 +54,13 @@ function getPlantingGardenSquaresCurrent(): Promise<Array<GardenSquarePlanting>>
 
 export {
   getPlantingDirectSowGardenRows,
+  addPlantingDirectSowGardenRow,
   getPlantingDirectSowGardenSquares,
+  addPlantingDirectSowGardenSquare,
   getPlantingSeedTrays,
+  addPlantingSeedTray,
   getPlantingTransplantedGardenSquares,
+  addPlantingTransplantedGardenSquare,
   getPlantingSeedTrayCurrent,
   getPlantingGardenSquaresCurrent
 }
